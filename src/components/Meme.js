ea893252bc7e7a22948a1fd840ce6e4fe5bfd3dc
Augusto-Carissimo <augusto.carissimo@gmail.com>
2024-01-30 19:11:08 -0300

    Add getRandomImgMeme from memeData.js

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,13 +1,36 @@
+import React from 'react';
 import '../index.css'
+import memeData from '../memeData.js'
 
 function Meme() {
+
+  const [meme, setMeme] = React.useState({
+    topText: '',
+    bottonText: '',
+    randomImage: 'Group 77.png'
+  })
+
+  const [allMemeImages, setAllMemeImages] = React.useState(memeData)
+
+  function getRandomImgMeme() {
+    const memeArray = allMemeImages.data.memes
+    const random = Math.floor(Math.random() * memeArray.length)
+    const url = memeArray[random].url
+    setMeme(prevState => ({
+      ...prevState,
+      randomImage: url
+    }))
+  }
+
   return(
     <main>
-      <form className='form'>
+      <div className='form'>
         <input type='text' placeholder='Top text' className='form-input'/>
         <input type='text' placeholder='Bottom text' className='form-input'/>
-        <button className='form-button'> Get a new image </button>
-      </form>
+        <button onClick={getRandomImgMeme} className='form-button'> Get a new image </button>
+      </div>
+
+      <img src={require("../images/memes/" + meme.randomImage)} className='meme-image'/>
     </main>
   )
 }
---
+++ src/memeData.js
@@ -0,0 +1,25 @@
+const memeData = {
+  "success": true,
+  "data": {
+    "memes": [
+      {
+        "id": "1",
+        "url": "Group 77.png"
+      },
+      {
+        "id": "2",
+        "url": "Rectangle 84.png"
+      },
+      {
+        "id": "3",
+        "url": "image-12.png"
+      },
+      {
+        "id": "4",
+        "url": "mountain-bike.png"
+      },
+    ]
+  }
+}
+
+export default memeData
